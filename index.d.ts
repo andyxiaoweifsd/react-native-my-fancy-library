e564f76e428c27162cef423fe78834f9668d8986
2244661996 <2244661996@qq.com>
2020-05-06 03:08:38 +0800

    1.0.3

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+import React, { PureComponent } from 'react';
+import { NativeModules, Text } from 'react-native';
+const { RNMyFancyLibrary } = NativeModules;
+export class Demos extends PureComponent {}
+export default RNMyFancyLibrary;
